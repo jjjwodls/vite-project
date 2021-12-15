890e772538956903a6345b932071ccba27d5382f
jjjwodls <jjjwodls@gmail.com>
2021-12-15 10:38:52 +0900

    eslint setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import React, { useState } from 'react'
-import { Header } from './App2'
+import React, { useState } from 'react';
+import { Header } from './App2';
 import Test from '@components/Test';
-import logo from './logo.svg'
-
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  const foo = 'tt';
   return (
     <Header>
       <Test></Test>
-      
     </Header>
-  )
+  );
 }
 
 export default App;
